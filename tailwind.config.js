85dda910df4316b000526d3d4fdae3d8a7fa104b
mohamedsamysaadoun3 <mohamedsamysaadoun3@gmail.com>
2025-05-17 04:09:12 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,10 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html", "./src/**/*.{jsx,js}"],
+  theme: {
+    extend: {
+      colors: { primary: "#047857" }
+    }
+  },
+  plugins: [],
+}
