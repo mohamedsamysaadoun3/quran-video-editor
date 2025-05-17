c8753567d7acf1595e787ba7580f1ace2a7e9ed1
mohamedsamysaadoun3 <mohamedsamysaadoun3@gmail.com>
2025-05-17 04:14:08 +0300

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,1 +1,11 @@
+export async function fetchReciters() {
+  const res = await fetch('https://api.quran.com/api/v4/resources/recitations?language=ar')
+  const { recitations } = await res.json()
+  return recitations
+}
 
+export async function fetchSurahs() {
+  const res = await fetch('https://api.quran.com/api/v4/chapters')
+  const { chapters } = await res.json()
+  return chapters
+}
